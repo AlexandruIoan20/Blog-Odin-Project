3cb866cc60c4d3f3cf3d7876ca8b504f13896e15
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-06-28 11:29:48 +0300

    No more .env in github

--- blog/models/user.js
+++ blog/models/user.js
@@ -15,6 +15,10 @@ const UserSchema = new Schema({
     image: { 
         type: String, 
     },
+    status: { 
+        type: [String], 
+        default: ['User'], 
+    }, 
     activity: { 
         posts: {
             type: [Schema.Types.ObjectId], 
