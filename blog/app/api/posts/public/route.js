958e4171e1fdbda889275de170ff812958a12d6f
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-06-30 13:44:21 +0300

    Post Card start"

--- blog/app/api/posts/public/route.js
+++ blog/app/api/posts/public/route.js
@@ -4,7 +4,7 @@ import Post from "@models/post";
 export const GET = async (req, res) => { 
     try { 
         await connectToDB();
-        const posts = await Post.find({ visibility: 'public'}); 
+        const posts = await Post.find({ visibility: 'public'}).populate('creator'); 
 
         if(posts.length === 0) { 
             return new Response("No posts available", { status: 200 }); 
--- blog/components/PostCard.jsx
+++ blog/components/PostCard.jsx
@@ -1,15 +1,34 @@
 'use client'; 
 
 import React from 'react'
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'; 
+import Link from 'next/link';
+import { useSession } from 'next-auth/react';
 
 const PostCard = ({ dev, post }) => {
-  const pathname = usePathname (); 
+  const { data: session } = useSession(); 
+  const pathName = usePathname (); 
   const router = useRouter (); 
+
   return (
-    <article>
-      <h2>{ post.title } </h2>
-    </article>
+    <div className='mb-4'>
+      <article className='bg-slate-200 px-6 pb-2 pt-2 mx-4 mt-2 rounded-2xl shadow-lg flex'>
+        <div>
+          <pre> {JSON.stringify(pathName) } </pre>
+          <h2 className='text-2xl font-inter'>{ post.title } </h2>
+          <p className='text-xs font-satoshi'> { post.creator.username } </p>
+        </div>
+        <div className='flex justify-end ml-auto'>
+          <button className='default_button'> View Post</button>
+        </div>
+      </article>
+      { dev && pathName ==  `/users/${session.user.id}` &&
+          <div className='mx-8 bg-slate-300 rounded-xl py-4 flex justify-center gap-x-64'>
+            <Link href = '/'>Edit</Link>
+            <Link href = '/'>Delete</Link>
+          </div>
+      }
+    </div>
   )
 }
 
